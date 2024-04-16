010d504dc0c3593b3eaaaee657388883cbc3c8b1
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-04-16 16:21:02 +0000

    a script that prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+--prepares a MySQL server for the project
+
+--creates the database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+--creates user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
+
+--grants privilidges
+GRANT ALL ON hbnb_dev_db.* TO hbnb_dev
+GRANT SELECT ON performance_schema.* TO hbnb_dev
