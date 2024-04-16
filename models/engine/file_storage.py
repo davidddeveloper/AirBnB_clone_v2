091bae6b5277f2be21c95de09aecb3f0e3d2c4bb
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-04-16 16:45:40 +0000

    started working on the filestorage to create a delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj):
+        if (obj == None):
+            return
+        
+        else:
+            pass
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,6 +6,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- creates user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
 
--- rants privilidges
+-- grants privilidges
 GRANT ALL ON hbnb_dev_db.* TO hbnb_dev
 GRANT SELECT ON performance_schema.* TO hbnb_dev
