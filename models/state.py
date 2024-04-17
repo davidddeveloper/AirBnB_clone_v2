5f4e75564f5f81d61515621b92b35551cadf93ea
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-04-17 14:50:45 +0000

    started setting up mysqlalchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(City, back_populates="state")
