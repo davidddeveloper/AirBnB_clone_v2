6d1ea2d65948790a715773129cd4acc2337b0867
davidddeveloper <programmingdonebydavid@gmail.com>
2024-04-17 04:36:32 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,8 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- creates user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY '@1Hbnb_dev_pwd';
 
 -- grants privilidges
-GRANT ALL ON hbnb_dev_db.* TO hbnb_dev
-GRANT SELECT ON performance_schema.* TO hbnb_dev
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
