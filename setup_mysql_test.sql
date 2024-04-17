6b89a157ffcb9b9e0bbff0aa68c84f62cf1fe291
davidddeveloper <programmingdonebydavid@gmail.com>
2024-04-17 04:41:23 -0700

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- prepares a MySQL server for the project
+
+-- creates the database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- creates user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY '@1Hbnb_test_pwd';
+
+-- grants privilidges
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
