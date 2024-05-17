bd8be4dee73e5d6776fa53c93e0a94228c316f4a
davidddeveloper <programmingdonebydavid@gmail.com>
2024-05-17 03:09:40 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-# a Fabric script that generates a .tgz archive from the contents of the web_static
-# using the function do_pack
+"""
+a Fabric script that generates a .tgz archive
+from the contents of the web_static directory
+using the function do_pack
+
+"""
+
 from fabric.api import local
 
 
 def do_pack():
-    "creating an archive"
+    "create a compressed archive from web_static directory"
     local("mkdir -p versions")
-    local('tar -czvf versions/web_static_$(date +"%Y%m%d%H%M%S").tgz web_static')
+    local(
+        'tar -czvf versions/web_static_$(date +"%Y%m%d%H%M%S").tgz web_static'
+    )
