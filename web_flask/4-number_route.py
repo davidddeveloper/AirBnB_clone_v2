3f5114ea9cfc66cc4936e5d572f1cf2faeb4b7ad
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-03 07:21:54 -0700

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+        listening on 0.0.0.0
+        port 5000
+
+    contain the following function
+    hello
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb_hello():
+    """ handle / route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ handles the /hbnb route """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hbnb_c(text):
+    """ takes a value and returns it """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>")
+def hbnb_python(text):
+    """ takes a value and returns it """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>")
+def hbnb_number():
+    """ takes a number and returns it
+        only if it is an integer """
+
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
