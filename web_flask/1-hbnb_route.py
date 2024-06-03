8587d807680086f81325dc0e13c92faaf3a2d3ff
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-03 07:01:24 -0700

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+        listening on 0.0.0.0
+        port 5000
+
+    contain the following function
+    hello
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb_hello():
+    """ handle / route """
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    """ handles the /hbnb route """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
