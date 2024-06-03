23ee4889a17e913547622e8ac365ef9983bb42d8
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-03 07:17:27 -0700

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+        listening on 0.0.0.0
+        port 5000
+
+    contain the following function
+    hello
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hbnb_hello():
+    """ handle / route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ handles the /hbnb route """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hbnb_c(text):
+    """ takes a value and returns it """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>")
+def hbnb_python():
+    """ takes a value and returns it """
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
