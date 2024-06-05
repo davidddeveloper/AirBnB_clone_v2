134e319386e5ff2f9b0f9cbb02eeb1a0878c7794
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-05 07:13:10 -0700

    cities attribute for FileStorage and cities column for DBStorage

--- models/state.py
+++ models/state.py
@@ -9,25 +9,29 @@ from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
+    from models.__init__ import storage
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship(City, back_populates="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """ Retrives cities associated with a specific state by """
-        from models.__init__ import storage
+    if storage.__class__.__name__ == 'DBStorage':
+        cities = relationship(City, back_populates="state", cascade="all, delete")
+    else:  # FileStorage
+        @property
+        def cities(self):
+            """ Retrives cities associated with a specific state by """
+            from models.__init__ import storage
 
-        all_cities = storage.all(City)
+            all_cities = storage.all(City)
 
-        # get cities associated with specific state
-        cities_in_state = []
-        for city_obj in all_cities.values():
-            try:
-                if city_obj.state_id == self.id:
-                    # city associated with state
-                    cities_in_state.append(city_obj)
-            except IndexError:
-                pass
+            # get cities associated with specific state
+            cities_in_state = []
+            for city_obj in all_cities.values():
+                try:
+                    if city_obj.state_id == self.id:
+                        # city associated with state
+                        cities_in_state.append(city_obj)
+                except IndexError:
+                    pass
 
-        return cities_in_state
+            return cities_in_state
